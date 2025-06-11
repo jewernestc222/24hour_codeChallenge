0d4a9285414cfeb8a80d747eb6919939e0248f19
jewernestc222 <jewernestc@gmail.com>
2025-06-11 14:22:48 +0800

    withworkingcode

--- importcsv.php
+++ importcsv.php
@@ -7,19 +7,12 @@ $pizza_type_id ="";
     $xy[] ="";
 if ( $handle = fopen( 'file_downloaded/pizza_types.csv', 'r' ) ) {
     while (($row = fgetcsv($handle, 1000, ",")) !== false) {
-      
       $pizza_type_id = str_replace(',', '', $row[0]);
       $name = str_replace(',', '', $row[1]);
       $category = str_replace(',', '', $row[2]);
       $ingredients = str_replace(',', '', $row[3]);
-      
       array_push($xy,(object)["pizza_type" => $pizza_type_id,"name" => $name, "category" => $category,"ingredients" => $ingredients]);
-
-      //$xy[] = ["pizza_type" => $pizza_type_id,"name" => $name, "category" => $category,"ingredients" => $ingredients];
-
-      
     }
-
 }
 echo json_encode($xy);
 ?>
